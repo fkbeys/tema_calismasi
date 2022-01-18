647cc37653b68406325e61898adf30def1a7b65d
fkbeys <fkbeys@gmail.com>
2022-01-19 01:52:36 +0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      /*ffffxx
+      /*xxxxxxxxxxxx
       /sasdasd
       // ignore: prefer_const_literals_to_create_immutables
       localizationsDelegates: [
